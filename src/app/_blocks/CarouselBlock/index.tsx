674ec01d669d7f0a9b168311b1f6ec51e9aa39ab
Nick Bevers <nick.bevers9@gmail.com>
2024-04-08 21:33:06 +0200

    feat: make rendering of controls and dots optional depending on props

--- src/app/_blocks/CarouselBlock/index.tsx
+++ src/app/_blocks/CarouselBlock/index.tsx
@@ -11,7 +11,13 @@ type Props = Extract<Page['layout'][0], { blockType: 'carouselBlock' }> & {
   id?: string
 }
 
-export const CarouselBlock: React.FC<Props> = ({ slides, position, height }) => {
+export const CarouselBlock: React.FC<Props> = ({
+  slides,
+  position,
+  height,
+  showArrows,
+  showDots,
+}) => {
   const [activeSlide, setActiveSlide] = useState<number>(0)
 
   const handlePrev = () => {
@@ -30,32 +36,45 @@ export const CarouselBlock: React.FC<Props> = ({ slides, position, height }) => 
     <div className={classes[position]}>
       <Gutter>
         <div className={classes.carousel} style={height ? { maxHeight: height } : {}}>
-          <div className={classes.controls}>
-            <svg
-              onClick={handlePrev}
-              className={classes.control}
-              aria-label="Previous slide"
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="#ffffff"
-              viewBox="0 0 256 256"
-            >
-              <path d="M168.49,199.51a12,12,0,0,1-17,17l-80-80a12,12,0,0,1,0-17l80-80a12,12,0,0,1,17,17L97,128Z"></path>
-            </svg>
-            <svg
-              onClick={handleNext}
-              className={classes.control}
-              aria-label="Next slide"
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="#ffffff"
-              viewBox="0 0 256 256"
-            >
-              <path d="M184.49,136.49l-80,80a12,12,0,0,1-17-17L159,128,87.51,56.49a12,12,0,1,1,17-17l80,80A12,12,0,0,1,184.49,136.49Z"></path>
-            </svg>
-          </div>
+          {showArrows && (
+            <div className={classes.controls}>
+              <svg
+                onClick={handlePrev}
+                className={classes.control}
+                aria-label="Previous slide"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                fill="#ffffff"
+                viewBox="0 0 256 256"
+              >
+                <path d="M168.49,199.51a12,12,0,0,1-17,17l-80-80a12,12,0,0,1,0-17l80-80a12,12,0,0,1,17,17L97,128Z"></path>
+              </svg>
+              <svg
+                onClick={handleNext}
+                className={classes.control}
+                aria-label="Next slide"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                fill="#ffffff"
+                viewBox="0 0 256 256"
+              >
+                <path d="M184.49,136.49l-80,80a12,12,0,0,1-17-17L159,128,87.51,56.49a12,12,0,1,1,17-17l80,80A12,12,0,0,1,184.49,136.49Z"></path>
+              </svg>
+            </div>
+          )}
+          {showDots && (
+            <div className={classes.dots}>
+              {slides.map((_, index) => (
+                <div
+                  key={index}
+                  className={`${classes.dot} ${activeSlide === index ? classes.active : ''}`}
+                  onClick={() => setActiveSlide(index)}
+                ></div>
+              ))}
+            </div>
+          )}
           <div className={classes.slides__container}>
             <div className={classes.slides}>
               {slides.map((slide, index) => (
